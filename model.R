69ca63fea3c80120ad6f5dc6d1ea2d7bd697e7a1
Michael Trittin <trittimo@rose-hulman.edu>
2018-03-30 11:58:49 -0400

    Initial commit

---
+++ model.R
@@ -0,0 +1,73 @@
+library(gmodels)
+library(e1071)
+library(stringi)
+library(SnowballC)
+library(tm)
+library(data.table)
+
+# Import our data
+reddit <- read.csv("reddit.csv", stringsAsFactors = FALSE)
+senticnet <- read.csv("senticnet.csv", stringsAsFactors = FALSE)
+
+# Types of analysis we can do:
+#   Point prediction
+#   Positivity rating
+
+# Breakdown of point prediction
+# Point values have been split into four categories:
+#   negative: points <= 0,
+#   low: points / maxscore_at_depth <= 0.3
+#   medium: points / maxscore_at_depth <= 0.8
+#   high: points / maxscore_at_depth > 0.8
+# We're using naive bayes to predict which of these categories a comment will fall in
+
+# Classify positivity as positive or negative
+senticnet$classified_score <- cut(senticnet$score, breaks = c(-1, 0, 1), right = FALSE, labels = c("Positive", "Negative"))
+
+# Determine the points ratio
+reddit$points_ratio <- reddit$points / reddit$maxscore_at_depth
+
+# Classify points as negative, low, medium, high
+reddit$classified_points <- cut(reddit$points_ratio,
+                                breaks = c(-1, 0, 0.3, 0.8, 1),
+                                right = FALSE,
+                                labels = c("negative", "low", "medium", "high"))
+
+# Prepare our reddit comments for analysis
+reddit_corpus <- VCorpus(VectorSource(reddit$comment))
+reddit_corpus_clean <- tm_map(reddit_corpus, content_transformer(tolower))
+reddit_corpus_clean <- tm_map(reddit_corpus_clean, content_transformer(stri_escape_unicode))
+reddit_corpus_clean <- tm_map(reddit_corpus_clean, removeWords, stopwords())
+reddit_corpus_clean <- tm_map(reddit_corpus_clean, removePunctuation)
+reddit_corpus_clean <- tm_map(reddit_corpus_clean, stemDocument)
+reddit_corpus_clean <- tm_map(reddit_corpus_clean, stripWhitespace)
+reddit_dtm <- DocumentTermMatrix(reddit_corpus_clean)
+
+# Create the training dataset
+reddit_dtm_train <- reddit_dtm[1:25, ]
+reddit_dtm_test <- reddit_dtm[26:50, ]
+
+reddit_train_labels <- reddit[1:25, ]$classified_points
+reddit_test_labels <- reddit[26:50, ]$classified_points
+
+# Eliminate words that don't appear in many comments
+# TODO: raise this value
+reddit_freq_terms <- findFreqTerms(reddit_dtm_train, 1)
+reddit_dtm_freq_train <- reddit_dtm_train[ , reddit_freq_terms]
+reddit_dtm_freq_test <- reddit_dtm_test[ , reddit_freq_terms]
+
+# Convert the counts into yes and no
+convert_counts <- function(x) {
+  x <- ifelse(x > 0, "Yes", "No")
+}
+
+reddit_train <- apply(reddit_dtm_freq_train, MARGIN = 2, convert_counts)
+reddit_test  <- apply(reddit_dtm_freq_test, MARGIN = 2, convert_counts)
+
+# Train the model on the data
+reddit_classifier <- naiveBayes(reddit_train, reddit_train_labels, laplace = 1)
+
+reddit_test_pred <- predict(reddit_classifier, reddit_test)
+
+# Print the results
+CrossTable(reddit_test_pred, reddit_test_labels, prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted', 'actual'))
